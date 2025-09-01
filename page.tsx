99d8b3cbe81334a21f434aa1637d85720cc1db01
wwyne <wwyne@wgu.edu>
2025-08-31 23:11:11 -0700

    Delete page.tsx

--- page.tsx
+++
@@ -1,2 +0,0 @@
-'use client';import React from 'react';import {Button} from '@/components/ui/button';
-export default function Page(){return <main className='p-10 text-center'><h1 className='text-4xl font-bold'>MarlboroMan Visuals</h1><p className='mt-4'>Cinematic Drone Services Website</p><Button className='mt-6'>Book a Flight</Button></main>;}
