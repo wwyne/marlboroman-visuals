d85d816a643a931c20cc7f3f1e382220c22ec3b8
wwyne <wwyne@wgu.edu>
2025-08-31 23:10:39 -0700

    Delete input.tsx

--- input.tsx
+++
@@ -1,2 +0,0 @@
-import * as React from 'react';import { cn } from '@/lib/utils';
-export const Input=React.forwardRef<HTMLInputElement,React.InputHTMLAttributes<HTMLInputElement>>(({className,...props},ref)=>(<input ref={ref} className={cn('border rounded px-3 py-2',className)} {...props}/>));
